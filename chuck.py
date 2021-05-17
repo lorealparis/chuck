e55c9d203aaecf59089a0702a8c8e10d8fae4058
lorealparis <lorenagb16@gmail.com>
2021-05-17 17:45:54 +0200

    bot con search

--- chuck.py
+++ chuck.py
@@ -5,11 +5,13 @@ from dotenv import load_dotenv
 import os
 import aiohttp
 from discord.ext import commands
+import random
 
-chucknorris_api = "https://api.chucknorris.io/jokes/random"
+chucknorris_random_api = "https://api.chucknorris.io/jokes/random"
+chucknorris_search_api = "https://api.chucknorris.io/jokes/search?query="
+
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-GUILD = int(os.getenv("DISCORD_GUILD"))
 print(os.getenv("USERNAME"))
 intents=discord.Intents.default()
 intents.members=True
@@ -18,11 +20,7 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print("Ready!")
-    for guild in client.guilds:
-        if guild.id == GUILD:
-            for member in guild.members:
-                print (member.name)
-            break
+
 
 @client.event
 async def on_message(message):
@@ -30,10 +28,20 @@ async def on_message(message):
         return
     if message.content.startswith("!random"):
         async with aiohttp.ClientSession() as session:
-            async with session.get(chucknorris_api) as response:
+            async with session.get(chucknorris_random_api) as response:
                 if response.status == 200:
                     js = await response.json()
                     await message.channel.send (js ["value"])
+        return
+
+    if message.content.startswith("!search"):
+        search_term = message.content[8:]
+        async with aiohttp.ClientSession() as session:
+            async with session.get(chucknorris_search_api+search_term) as response:
+                if response.status == 200:
+                    js = await response.json()
+                    await message.channel.send (random.choice(js["result"])["value"])
+        return
 
 
    
